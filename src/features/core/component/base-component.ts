52950dd5c983eeeebbfbffda05a29985e1e6161e
Vyacheslav Morozov <blurryfacebeat@gmail.com>
2025-07-18 03:07:54 +0300

    feat: add base-component class

---
+++ src/features/core/component/base-component.ts
@@ -0,0 +1,35 @@
+export abstract class BaseComponent<TElement extends Element = Element> {
+  protected element!: TElement;
+  protected isMounted = false;
+
+  abstract render(): TElement;
+  abstract update(): void;
+
+  protected onMount(): void {}
+  protected onDestroy(): void {}
+
+  mount(parent: Element): void {
+    if (this.isMounted) {
+      return;
+    }
+
+    this.element = this.render();
+    parent.appendChild(this.element);
+    this.onMount();
+    this.isMounted = true;
+  }
+
+  destroy(): void {
+    if (!this.isMounted) {
+      return;
+    }
+
+    this.element.remove();
+    this.onDestroy();
+    this.isMounted = false;
+  }
+
+  getElement(): TElement {
+    return this.element;
+  }
+}
